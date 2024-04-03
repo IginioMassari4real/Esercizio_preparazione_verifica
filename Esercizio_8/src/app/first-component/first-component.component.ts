0b0462e3db3e961175ed133793c8283f9c944918
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-04-03 07:49:17 +0000

    progetto finito

--- Esercizio_8/src/app/first-component/first-component.component.ts
+++ Esercizio_8/src/app/first-component/first-component.component.ts
@@ -16,35 +16,37 @@ export class FirstComponentComponent {
     
   }
   setBtn(nomeT: string){
-    if(nomeT == "#120 STARYU" ){
-      this.name = "#120 STARYU";
+    if(nomeT == "MILANO" ){
+      this.name = "MILANO";
     }
-    if(nomeT == "#304 ARON" ){
-      this.name = "#304 ARON";
+    if(nomeT == "ROMA" ){
+      this.name = "ROMA";
     }
-    if(nomeT == "#539 SAWK" ){
-      this.name = "#539 SAWK";
+    if(nomeT == "NAPOLI" ){
+      this.name = "NAPOLI";
     }
-    if(nomeT == "#120 STARYU" ){
-      this.name = "#120 STARYU";
-
-    if(nomeT == "#120 STARYU" ){
-      this.name = "#120 STARYU";
+    if(nomeT == "VENEZIA" ){
+      this.name = "VENEZIA";
     }
-    
-
+    if(nomeT == "BOLOGNA" ){
+      this.name = "BOLOGNA";
+    }
+  
+  }
     posti = [
 
-      {nome:"Milano", nazione:"Italia"},
+      {nome:"MILANO", nazione:"Italia", immagine:"https://media.istockphoto.com/id/493223692/it/foto/duomo-di-milano-italia.jpg?s=612x612&w=0&k=20&c=X8QrI64NuylVeasXYRnKoj2LEHJNgI5OcVwoQwzd0bo=", capitale:"Roma", abitanti:"1.371.850" },
 
-      {nome:"Roma", nazione:"Italia"},
+      {nome:"ROMA", nazione:"Italia", immagine:"https://media.istockphoto.com/id/539115110/it/foto/colosseo-di-roma-e-sole-mattutino-italia.jpg?s=612x612&w=0&k=20&c=ngbBMGVEkJxHsnt4SN7ZuncEnRenq2tFI8V0-zCg4pw=", capitale:"Roma", abitanti:"2.848.084"},
 
-      {nome:"Napoli", nazione:"Italia"},
+      {nome:"NAPOLI", nazione:"Italia", immagine:"https://media.istockphoto.com/id/527072683/it/foto/citt%C3%A0-di-napoli-con-il-vesuvio-al-tramonto-campania-italia.jpg?s=612x612&w=0&k=20&c=Ba8KpBUWlvfj4RqyDJvYlG2NfNZARsccYR4kMST8Jb4=", capitale:"Roma", abitanti:"1.004.500"},
 
-      {nome:"Venezia", nazione:"Italia"},
+      {nome:"VENEZIA", nazione:"Italia", immagine:"https://media.istockphoto.com/id/1388018793/it/foto/canal-grande-a-venezia.jpg?s=612x612&w=0&k=20&c=1_Vi0rvZBiCO9SCZfJsHkDJsw-9qjLhcGiYyE-AAnpo=", capitale:"Roma", abitanti:"250.369"},
 
-      {nome:"Bologna", nazione:"Italia"},
+      {nome:"BOLOGNA", nazione:"Italia", immagine:"https://media.istockphoto.com/id/1070080094/it/foto/bologna-emilia-romagna-italia.jpg?s=612x612&w=0&k=20&c=dJqUQ_tBOZJijZwbuMOXh5JNAe63VrkW1rlMlIA1zC4=", capitale:"Roma", abitanti:"390.518"},
 
     ]
-
+  
 }
+
+  
